f3d681189abb6465b15cb13683723f8d3e205275
Mikko Saari <mikko@mikkosaari.fi>
2018-05-07 14:40:59 +0300

    Permalink fix.

--- lib/common.php
+++ lib/common.php
@@ -9,218 +9,6 @@
  */
 
 /**
- * Gets the next key-direction pair from the orderby array.
- *
- * Fetches a key-direction pair from the orderby array. Converts key names to match
- * the post object parameters when necessary and seeds the random generator, if
- * required.
- *
- * @param array $orderby An array of key-direction pairs.
- *
- * @return array A set of 'key', 'dir' for direction and 'compare' for proper
- * comparison method.
- */
-function relevanssi_get_next_key( &$orderby ) {
-	if ( ! is_array( $orderby ) || count( $orderby ) < 1 ) {
-		// Nothing to see here.
-		return array(
-			'key'     => null,
-			'dir'     => null,
-			'compare' => null,
-		);
-	}
-
-	list( $key ) = array_keys( $orderby );
-	$dir         = $orderby[ $key ];
-	unset( $orderby[ $key ] );
-
-	if ( 'rand' === strtolower( $dir ) ) {
-		$key = 'rand';
-	}
-
-	// Correcting the key for couple of shorthand cases.
-	switch ( $key ) {
-		case 'title':
-			$key = 'post_title';
-			break;
-		case 'date':
-			$key = 'post_date';
-			break;
-		case 'modified':
-			$key = 'post_modified';
-			break;
-		case 'parent':
-			$key = 'post_parent';
-			break;
-		case 'type':
-			$key = 'post_type';
-			break;
-		case 'name':
-			$key = 'post_name';
-			break;
-		case 'author':
-			$key = 'post_author';
-			break;
-		case 'relevance':
-			$key = 'relevance_score';
-			break;
-	}
-
-	$numeric_keys = array( 'menu_order', 'ID', 'post_parent', 'post_author', 'comment_count', 'relevance_score' );
-	$date_keys    = array( 'post_date', 'post_date_gmt', 'post_modified', 'post_modified_gmt' );
-
-	$compare = 'string';
-	if ( in_array( $key, $numeric_keys, true ) ) {
-		$compare = 'number';
-	} elseif ( in_array( $key, $date_keys, true ) ) {
-		$compare = 'date';
-	}
-
-	if ( 'rand' === $key ) {
-		if ( is_numeric( $dir ) ) {
-			// A specific random seed is requested.
-			mt_srand( $dir );
-		}
-	} else {
-		$dir = strtolower( $dir );
-		if ( 'asc' !== $dir ) {
-			$dir = 'desc';
-		}
-	}
-
-	$values = array(
-		'key'     => $key,
-		'dir'     => $dir,
-		'compare' => $compare,
-	);
-	return $values;
-}
-
-/**
- * Gets the values for comparing items for given key.
- *
- * Fetches the key values for the item pair. If random order is required, this
- * function will randomize the order.
- *
- * @global object $wp_query The global WP_Query object.
- *
- * @param string $key    The key used.
- * @param object $item_1 The first post object to compare.
- * @param object $item_2 The second post object to compare.
- *
- * @return array Array with the key values: 'key1' and 'key2', respectively.
- */
-function relevanssi_get_compare_values( $key, $item_1, $item_2 ) {
-	if ( 'rand' === $key ) {
-		do {
-			$key1 = rand();
-			$key2 = rand();
-		} while ( $key1 === $key2 );
-		$keys = array(
-			'key1' => $key1,
-			'key2' => $key2,
-		);
-		return $keys;
-	}
-
-	$key1 = '';
-	$key2 = '';
-
-	if ( 'meta_value' === $key || 'meta_value_num' === $key ) {
-		global $wp_query;
-		// Get the name of the field from the global WP_Query.
-		$key = $wp_query->query_vars['meta_key'];
-		if ( ! isset( $key ) ) {
-			// The key is not set.
-			return array( '', '' );
-		}
-
-		$key1 = get_post_meta( $item_1->ID, $key, true );
-		if ( empty( $key1 ) ) {
-			/**
-			 * Adds in a missing sorting value.
-			 *
-			 * In some cases the sorting method may not have values for all posts
-			 * (for example when sorting by 'menu_order'). If you still want to use
-			 * a sorting method like this, you can use this function to fill in a
-			 * value (in the case of 'menu_order', for example, one could use
-			 * PHP_INT_MAX.)
-			 *
-			 * @param string $key1 The value to filter.
-			 * @param string $key  The name of the key.
-			 */
-			$key1 = apply_filters( 'relevanssi_missing_sort_key', $key1, $key );
-		}
-
-		$key2 = get_post_meta( $item_2->ID, $key, true );
-		if ( empty( $key2 ) ) {
-			/**
-			 * Documented in lib/common.php.
-			 */
-			$key2 = apply_filters( 'relevanssi_missing_sort_key', $key2, $key );
-		}
-	} else {
-		if ( isset( $item_1->$key ) ) {
-			$key1 = relevanssi_strtolower( $item_1->$key );
-		} else {
-			/**
-			 * Documented in lib/common.php.
-			 */
-			$key1 = apply_filters( 'relevanssi_missing_sort_key', $key1, $key );
-		}
-		if ( isset( $item_2->$key ) ) {
-			$key2 = relevanssi_strtolower( $item_2->$key );
-		} else {
-			/**
-			 * Documented in lib/common.php.
-			 */
-			$key2 = apply_filters( 'relevanssi_missing_sort_key', $key2, $key );
-		}
-	}
-
-	$keys = array(
-		'key1' => $key1,
-		'key2' => $key2,
-	);
-	return $keys;
-}
-
-/**
- * Compares to values.
- *
- * Compares two sorting keys using date based comparison, string comparison or
- * numeric comparison.
- *
- * @param string $key1 The first key.
- * @param string $key2 The second key.
- * @param string $compare The comparison method; possible values are 'date' for
- * date comparisons and 'string' for string comparison, everything else is
- * considered a numeric comparison.
- *
- * @return int $val Returns < 0 if key1 is less than key2; > 0 if key1 is greater
- * than key2, and 0 if they are equal.
- */
-function relevanssi_compare_values( $key1, $key2, $compare ) {
-	$val = 0;
-	if ( 'date' === $compare ) {
-		if ( strtotime( $key1 ) > strtotime( $key2 ) ) {
-			$val = 1;
-		} elseif ( strtotime( $key1 ) < strtotime( $key2 ) ) {
-			$val = -1;
-		}
-	} elseif ( 'string' === $compare ) {
-		$val = relevanssi_mb_strcasecmp( $key1, $key2 );
-	} else {
-		if ( $key1 > $key2 ) {
-			$val = 1;
-		} elseif ( $key1 < $key2 ) {
-			$val = -1;
-		}
-	}
-	return $val;
-}
-
-/**
  * Multibyte friendly case-insensitive string comparison.
  *
  * If multibyte string functions are available, do strcmp() after using
@@ -260,86 +48,6 @@ function relevanssi_strtolower( $string ) {
 	} else {
 		return mb_strtolower( $string );
 	}
-}
-
-/**
- * Compares values using multiple levels of sorting keys.
- *
- * Comparison function for usort() using multiple levels of sorting methods. If one
- * level produces a tie, the sort will get a next level of sorting methods.
- *
- * @global array $relevanssi_keys     An array of sorting keys by level.
- * @global array $relevanssi_dirs     An array of sorting directions by level.
- * @global array $relevanssi_compares An array of comparison methods by level.
- *
- * @param object $a A post object.
- * @param object $b A post object.
- *
- * @return int $val Returns < 0 if a is less than b; > 0 if a is greater
- * than b, and 0 if they are equal.
- */
-function relevanssi_cmp_function( $a, $b ) {
-	global $relevanssi_keys, $relevanssi_dirs, $relevanssi_compares;
-
-	$level = -1;
-	$val   = 0;
-
-	while ( 0 === $val ) {
-		$level++;
-		if ( ! isset( $relevanssi_keys[ $level ] ) ) {
-			// No more levels; we've hit the bedrock.
-			$level--;
-			break;
-		}
-		$compare        = $relevanssi_compares[ $level ];
-		$compare_values = relevanssi_get_compare_values( $relevanssi_keys[ $level ], $a, $b );
-		$val            = relevanssi_compare_values( $compare_values['key1'], $compare_values['key2'], $compare );
-	}
-
-	if ( 'desc' === $relevanssi_dirs[ $level ] ) {
-		$val = $val * -1;
-	}
-
-	return $val;
-}
-
-/**
- * Sorts post objects.
- *
- * Sorts post objects using multiple levels of sorting methods. This function was
- * originally written by Matthew Hood and published in the PHP manual comments.
- * The actual sorting is handled by relevanssi_cmp_function().
- *
- * @global array $relevanssi_keys     An array of sorting keys by level.
- * @global array $relevanssi_dirs     An array of sorting directions by level.
- * @global array $relevanssi_compares An array of comparison methods by level.
- *
- * @param array $data    The posts to sort are in $data[0], used as a reference.
- * @param array $orderby The array of orderby rules with directions.
- */
-function relevanssi_object_sort( &$data, $orderby ) {
-	global $relevanssi_keys, $relevanssi_dirs, $relevanssi_compares;
-
-	$relevanssi_keys     = array();
-	$relevanssi_dirs     = array();
-	$relevanssi_compares = array();
-
-	do {
-		$values = relevanssi_get_next_key( $orderby );
-		if ( ! empty( $values['key'] ) ) {
-			$relevanssi_keys[]     = $values['key'];
-			$relevanssi_dirs[]     = $values['dir'];
-			$relevanssi_compares[] = $values['compare'];
-		}
-	} while ( ! empty( $values['key'] ) );
-
-	$primary_key = $relevanssi_keys[0];
-	if ( ! isset( $data[0]->$primary_key ) ) {
-		// Trying to sort by a non-existent key.
-		return;
-	}
-
-	usort( $data, 'relevanssi_cmp_function' );
 }
 
 /**
@@ -417,115 +125,6 @@ function relevanssi_show_matches( $data, $hit ) {
 	 * @param string $result The breakdown.
 	 */
 	return apply_filters( 'relevanssi_show_matches', $result );
-}
-
-/**
- * Adds the search query to the log.
- *
- * Logs the search query, trying to avoid bots.
- *
- * @global object $wpdb                 The WordPress database interface.
- * @global array  $relevanssi_variables The global Relevanssi variables, used for database table names.
- *
- * @param string $query The search query.
- * @param int    $hits  The number of hits found.
- */
-function relevanssi_update_log( $query, $hits ) {
-	// Bot filter, by Justin_K.
-	// See: http://wordpress.org/support/topic/bot-logging-problem-w-tested-solution.
-	$user_agent = '';
-	if ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
-		$user_agent = $_SERVER['HTTP_USER_AGENT'];
-		$bots       = array( 'Google' => 'Mediapartners-Google' );
-
-		/**
-		 * Filters the bots Relevanssi should block from logs.
-		 *
-		 * Lets you filter the bots that are blocked from Relevanssi logs.
-		 *
-		 * @param array $bots An array of bot user agents.
-		 */
-		$bots = apply_filters( 'relevanssi_bots_to_not_log', $bots );
-		foreach ( $bots as $name => $lookfor ) {
-			if ( false !== stristr( $user_agent, $lookfor ) ) {
-				return;
-			}
-		}
-	}
-
-	/**
-	 * Filters the current user for logs.
-	 *
-	 * The current user is checked before logging a query to omit particular users.
-	 * You can use this filter to filter out the user.
-	 *
-	 * @param object The current user object.
-	 */
-	$user = apply_filters( 'relevanssi_log_get_user', wp_get_current_user() );
-	if ( 0 !== $user->ID && get_option( 'relevanssi_omit_from_logs' ) ) {
-		$omit = explode( ',', get_option( 'relevanssi_omit_from_logs' ) );
-		if ( in_array( strval( $user->ID ), $omit, true ) ) {
-			return;
-		}
-		if ( in_array( $user->user_login, $omit, true ) ) {
-			return;
-		}
-	}
-
-	$ip = '';
-	if ( 'on' === get_option( 'relevanssi_log_queries_with_ip' ) ) {
-		/**
-		 * Filters the IP address of the searcher.
-		 *
-		 * Relevanssi may store the IP address of the searches in the logs. If the
-		 * setting is enabled, this filter can be used to filter out the IP address
-		 * before the log entry is made.
-		 *
-		 * Do note that storing the IP address may be illegal or get you in GDPR
-		 * trouble.
-		 *
-		 * @param string $ip The IP address, from $_SERVER['REMOTE_ADDR'].
-		 */
-		$ip = apply_filters( 'relevanssi_remote_addr', $_SERVER['REMOTE_ADDR'] );
-	}
-
-	/**
-	 * Filters whether a query should be logged or not.
-	 *
-	 * This filter can used to determine whether a query should be logged or not.
-	 *
-	 * @param boolean $ok_to_log  Can the query be logged.
-	 * @param string  $query      The actual query string.
-	 * @param int     $hits       The number of hits found.
-	 * @param string  $user_agent The user agent that made the search.
-	 * @param string  $ip         The IP address the search came from (or empty).
-	 */
-	$ok_to_log = apply_filters( 'relevanssi_ok_to_log', true, $query, $hits, $user_agent, $ip );
-	if ( $ok_to_log ) {
-		global $wpdb, $relevanssi_variables;
-
-		$wpdb->query(
-			$wpdb->prepare( 'INSERT INTO ' . $relevanssi_variables['log_table'] . ' (query, hits, user_id, ip, time) VALUES (%s, %d, %d, %s, NOW())',
-			$query, intval( $hits ), $user->ID, $ip )
-		); // WPCS: unprepared SQL ok, Relevanssi database table name.
-	}
-}
-
-/**
- * Trims Relevanssi log table.
- *
- * Trims Relevanssi log table, using the day interval setting from 'relevanssi_trim_logs'.
- *
- * @global object $wpdb                 The WordPress database interface.
- * @global array  $relevanssi_variables The global Relevanssi variables, used for database table names.
- */
-function relevanssi_trim_logs() {
-	global $wpdb, $relevanssi_variables;
-	$interval = intval( get_option( 'relevanssi_trim_logs' ) );
-	$wpdb->query(
-		$wpdb->prepare( 'DELETE FROM ' . $relevanssi_variables['log_table'] . ' WHERE time < TIMESTAMP(DATE_SUB(NOW(), INTERVAL %d DAY))',
-		$interval )
-	); // WPCS: unprepared SQL ok, Relevanssi database table name.
 }
 
 /**
@@ -814,22 +413,6 @@ function relevanssi_strip_invisibles( $text ) {
 }
 
 /**
- * Sorts strings by length.
- *
- * A sorting function that sorts strings by length. Uses relevanssi_strlen() to
- * count the string length.
- *
- * @param string $a String A.
- * @param string $b String B.
- *
- * @return int Negative value, if string A is longer; zero, if strings are equally
- * long; positive, if string B is longer.
- */
-function relevanssi_strlen_sort( $a, $b ) {
-	return relevanssi_strlen( $b ) - relevanssi_strlen( $a );
-}
-
-/**
  * Returns the custom fields to index.
  *
  * Returns a list of custom fields to index, based on the custom field indexing
@@ -1104,24 +687,6 @@ function relevanssi_prevent_default_request( $request, $query ) {
 		}
 	}
 	return $request;
-}
-
-/**
- * Disables Relevanssi in the ACF Relationship field post search.
- *
- * We don't want to use Relevanssi on the ACF Relationship field post searches, so
- * this function disables it (on the 'relevanssi_search_ok' hook).
- *
- * @param boolean $search_ok Block the search or not.
- *
- * @return boolean False, if this is an ACF Relationship field search, pass the
- * parameter unchanged otherwise.
- */
-function relevanssi_acf_relationship_fields( $search_ok ) {
-	if ( isset( $_REQUEST['action'] ) && 'acf' === substr( $_REQUEST['action'], 0, 3 ) ) { // WPCS: CSRF ok.
-		$search_ok = false;
-	}
-	return $search_ok;
 }
 
 /**
@@ -1671,19 +1236,22 @@ function relevanssi_the_permalink() {
  *
  * @global object $post The global post object.
  *
- * @param string $link      The link to adjust.
- * @param object $link_post The post to modify. If null, use global $post.
- * Defaults null.
+ * @param string     $link      The link to adjust.
+ * @param object|int $link_post The post to modify, either WP post object or the post
+ * ID. If null, use global $post. Defaults null.
  *
  * @return string The modified link.
  */
 function relevanssi_permalink( $link, $link_post = null ) {
 	if ( null === $link_post ) {
 		global $post;
+		$link_post = $post;
+	} elseif ( is_int( $link_post ) ) {
+		$link_post = get_post( $link_post );
 	}
 	// Using property_exists() to avoid troubles from magic variables.
-	if ( is_object( $post ) && property_exists( $post, 'relevanssi_link' ) ) {
-		$link = $post->relevanssi_link;
+	if ( is_object( $link_post ) && property_exists( $link_post, 'relevanssi_link' ) ) {
+		$link = $link_post->relevanssi_link;
 	}
 	if ( is_search() ) {
 		$link = relevanssi_add_highlight( $link );
