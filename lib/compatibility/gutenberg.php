cbd3e8429ca99757f705777a930a985ef3bce906
Mikko Saari <mikko@mikkosaari.fi>
2019-04-08 09:23:36 +0300

    In Premium, remove duplicate post meta.

--- lib/compatibility/gutenberg.php
+++ lib/compatibility/gutenberg.php
@@ -10,6 +10,12 @@
  * @see     https://www.relevanssi.com/
  */
 
+if ( RELEVANSSI_PREMIUM ) {
+	// Gutenberg causes duplicate postmeta to appear in posts. This will remove the
+	// extras when a post is saved.
+	add_action( 'save_post', 'relevanssi_remove_duplicate_postmeta', 100 );
+}
+
 add_filter( 'relevanssi_post_content', 'relevanssi_gutenberg_block_rendering', 10, 2 );
 
 /**
