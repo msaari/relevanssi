a569c826a4ab26ab3cc7be244f4a16debbede5e7
Mikko Saari <mikko@mikkosaari.fi>
2019-06-07 07:04:43 +0300

    Test init functions.

---
+++ tests/test-init.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Class InitTest
+ *
+ * @package Relevanssi_Premium
+ * @author  Mikko Saari
+ */
+
+/**
+ * Test Relevanssi init functions pages.
+ *
+ * @group new
+ */
+class InitTest extends WP_UnitTestCase {
+	/**
+	 * Sets up the test.
+	 */
+	public static function wpSetUpBeforeClass() {
+		relevanssi_install();
+
+		// Truncate the index.
+		// relevanssi_truncate_index();
+	}
+
+	/**
+	 * Tests that the admin init function installs the required filters.
+	 *
+	 * @global array $relevanssi_variables
+	 */
+	public function test_admin_init() {
+		global $relevanssi_variables;
+
+		relevanssi_admin_init();
+		$this->assertNotFalse( has_filter( 'admin_enqueue_scripts', 'relevanssi_add_admin_scripts' ) );
+		$this->assertNotFalse(
+			has_filter(
+				'plugin_action_links_' . $relevanssi_variables['plugin_basename'],
+				'relevanssi_action_links'
+			)
+		);
+	}
+
+	/**
+	 *
+	 */
+	/**
+	 * Uninstalls Relevanssi.
+	 */
+	public static function wpTearDownAfterClass() {
+		require_once dirname( dirname( __FILE__ ) ) . '/lib/uninstall.php';
+		require_once dirname( dirname( __FILE__ ) ) . '/premium/uninstall.php';
+
+		if ( function_exists( 'relevanssi_uninstall' ) ) {
+			relevanssi_uninstall();
+		}
+		if ( function_exists( 'relevanssi_uninstall_free' ) ) {
+			relevanssi_uninstall_free();
+		}
+	}
+}
