a6f00c2096804a741183ff16213b3162bde1118d
Mikko Saari <mikko@mikkosaari.fi>
2019-08-13 10:25:56 +0300

    Version number update.

--- relevanssi.php
+++ relevanssi.php
@@ -13,7 +13,7 @@
  * Plugin Name: Relevanssi
  * Plugin URI: https://www.relevanssi.com/
  * Description: This plugin replaces WordPress search with a relevance-sorting search.
- * Version: 4.2.0
+ * Version: 4.3.0
  * Author: Mikko Saari
  * Author URI: http://www.mikkosaari.fi/
  * Text Domain: relevanssi
