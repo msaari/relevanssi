0f948eb44cfbfa7f69f9b74a59d26cb1d4d8b034
Mikko Saari <mikko@mikkosaari.fi>
2019-09-25 07:43:23 +0300

    Feature: Exclude Gutenberg blocks with a CSS class.
    You can now give Gutenberg blocks a CSS class "relevanssi_noindex" to exclude them from being indexed. Relevanssi will not index Gutenberg blocks that have the class.

--- lib/compatibility/gutenberg.php
+++ lib/compatibility/gutenberg.php
@@ -19,15 +19,35 @@ if ( RELEVANSSI_PREMIUM ) {
 add_filter( 'relevanssi_post_content', 'relevanssi_gutenberg_block_rendering', 10 );
 
 /**
- * Renders Gutenberg reusable blocks.
+ * Renders Gutenberg blocks.
  *
- * Gutenberg Reusable Blocks appear as comments in the post content. This function
- * picks up the comments and renders the blocks.
+ * Renders all sorts of Gutenberg blocks, including reusable blocks and ACF
+ * blocks. Also enables basic Gutenberg deindexing: you can add an extra CSS
+ * class 'relevanssi_noindex' to a block to stop it from being indexed by
+ * Relevanssi. This function is essentially the same as core do_blocks().
+ *
+ * @see do_blocks()
  *
  * @param string $content The post content.
  *
  * @return string The post content with the rendered content added.
  */
 function relevanssi_gutenberg_block_rendering( $content ) {
-	return do_blocks( $content );
+	$blocks = parse_blocks( $content );
+	$output = '';
+
+	foreach ( $blocks as $block ) {
+		if ( ! isset( $block['attrs']['className'] ) || strstr( $block['attrs']['className'], 'relevanssi_noindex' ) === false ) {
+			$output .= render_block( $block );
+		}
+	}
+
+	// If there are blocks in this content, we shouldn't run wpautop() on it later.
+	$priority = has_filter( 'the_content', 'wpautop' );
+	if ( false !== $priority && doing_filter( 'the_content' ) && has_blocks( $content ) ) {
+		remove_filter( 'the_content', 'wpautop', $priority );
+		add_filter( 'the_content', '_restore_wpautop_hook', $priority + 1 );
+	}
+
+	return $output;
 }
