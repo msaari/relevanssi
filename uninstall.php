abe54847f2e2eecf15b1f0d1d2698fbb8248e7ac
Mikko Saari <mikko@mikkosaari.fi>
2019-12-10 06:57:44 +0200

    Fix: Avoids overriding WP globals
    Just to remove a complaint.

--- uninstall.php
+++ uninstall.php
@@ -14,15 +14,15 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 if ( ! defined( 'RELEVANSSI_PREMIUM' ) ) {
-	define( 'RELEVANSSI_PREMIUM', false );	
+	define( 'RELEVANSSI_PREMIUM', false );
 }
 require_once 'lib/uninstall.php';
 
 if ( function_exists( 'is_multisite' ) && is_multisite() ) {
 	$blogids    = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 	$old_blogid = $wpdb->blogid;
-	foreach ( $blogids as $blog_id ) {
-		switch_to_blog( $blog_id );
+	foreach ( $blogids as $uninstall_blog_id ) {
+		switch_to_blog( $uninstall_blog_id );
 		relevanssi_uninstall_free();
 	}
 	switch_to_blog( $old_blogid );
