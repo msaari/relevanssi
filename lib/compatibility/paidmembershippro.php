4cb3f6544cce4336e257b342e909908e6b7eb5d5
Mikko Saari <mikko@mikkosaari.fi>
2020-11-09 15:29:39 +0200

    Fix: Better Paid Membership Pro compatibility
    Notice the pmpro_filterqueries setting.

--- lib/compatibility/paidmembershippro.php
+++ lib/compatibility/paidmembershippro.php
@@ -22,12 +22,16 @@ add_filter( 'relevanssi_post_ok', 'relevanssi_paidmembershippro_compatibility', 
  * otherwise false.
  */
 function relevanssi_paidmembershippro_compatibility( $post_ok, $post_id ) {
-	$status = relevanssi_get_post_status( $post_id );
+	$pmpro_active = get_option( 'pmpro_filterqueries', 0 );
 
-	if ( 'publish' === $status ) {
-		// Only apply to published posts, don't apply to drafts.
-		$current_user = wp_get_current_user();
-		$post_ok      = pmpro_has_membership_access( $post_id, $current_user->ID );
+	if ( $pmpro_active ) {
+		$status = relevanssi_get_post_status( $post_id );
+
+		if ( 'publish' === $status ) {
+			// Only apply to published posts, don't apply to drafts.
+			$current_user = wp_get_current_user();
+			$post_ok      = pmpro_has_membership_access( $post_id, $current_user->ID );
+		}
 	}
 
 	return $post_ok;
