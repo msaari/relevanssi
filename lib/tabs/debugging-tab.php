5e6b273874c38a746d3cefe55cd7d1c52968ef35
Mikko Saari <mikko@mikkosaari.fi>
2020-11-18 11:04:58 +0200

    Fix: Removes the "Buy Premium" prompt from debugging tab for Premium users

--- lib/tabs/debugging-tab.php
+++ lib/tabs/debugging-tab.php
@@ -28,8 +28,12 @@ function relevanssi_debugging_tab() {
 	<h2><?php esc_html_e( 'Debugging', 'relevanssi' ); ?></h2>
 
 	<p><?php esc_html_e( 'In order to figure out problems with indexing posts, you can test how Relevanssi sees the post by entering the post ID number in the field below.', 'relevanssi' ); ?></p>
-	<?php // Translators: %1$s starts the link, %2$s closes it. ?>
-	<p><?php printf( esc_html__( 'In Relevanssi Premium, you can find this feature for each post on the post edit page. %1$sBuy Relevanssi Premium here%2$s.', 'relevanssi' ), '<a href="https://www.relevanssi.com/buy-premium/">', '</a>' ); ?></p>
+	<?php
+	if ( ! RELEVANSSI_PREMIUM ) {
+		// Translators: %1$s starts the link, %2$s closes it.
+		printf( '<p>' . esc_html__( 'In Relevanssi Premium, you can find this feature for each post on the post edit page. %1$sBuy Relevanssi Premium here%2$s.', 'relevanssi' ) . '</p>', '<a href="https://www.relevanssi.com/buy-premium/">', '</a>' );
+	}
+	?>
 	<p><label for="post_id"><?php esc_html_e( 'The post ID', 'relevanssi' ); ?></label>:
 	<input type="text" name="post_id" id="post_id"
 	<?php
