9fc2cf607f0e9f0acc0215c0c6801881dc178d84
Mikko Saari <mikko@mikkosaari.fi>
2022-08-18 10:42:20 +0300

    Fix: CSS parameter initial value fix
    "text-color" should be "color".

--- lib/install.php
+++ lib/install.php
@@ -76,7 +76,7 @@ function _relevanssi_install() {
 	add_option( 'relevanssi_class', 'relevanssi-query-term' );
 	add_option( 'relevanssi_comment_boost', $relevanssi_variables['comment_boost_default'] );
 	add_option( 'relevanssi_content_boost', $relevanssi_variables['content_boost_default'] );
-	add_option( 'relevanssi_css', 'text-decoration: underline; text-color: #ff0000' );
+	add_option( 'relevanssi_css', 'text-decoration: underline; color: #ff0000' );
 	add_option( 'relevanssi_db_version', '0' );
 	add_option( 'relevanssi_default_orderby', 'relevance' );
 	add_option( 'relevanssi_disable_or_fallback', 'off' );
