5f26ae616f1ffc805c25587617577c94e216ff2a
Mikko Saari <mikko@mikkosaari.fi>
2025-12-16 05:24:33 +0200

    Fix: relevanssi_get_post_object() null values
    If relevanssi_get_post_object() gets a null ID, return WP_Error.

--- lib/utils.php
+++ lib/utils.php
@@ -598,6 +598,9 @@ function relevanssi_get_post_meta_for_all_posts( array $post_ids, string $field 
  */
 function relevanssi_get_post_object( $post_id ) {
 	$object = null;
+	if ( ! $post_id ) {
+		return new WP_Error( 'null_value', 'Null post ID' );
+	}
 	if ( '*' === substr( $post_id, 0, 1 ) ) {
 		// Convert from **type**id to a user or a term object.
 		$parts = explode( '**', $post_id );
